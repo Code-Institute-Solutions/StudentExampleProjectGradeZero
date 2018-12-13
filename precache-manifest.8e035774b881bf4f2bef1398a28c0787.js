b8224076d315436c6828a004b3932f410df975f0
Haley Schafer <hschafer@umich.edu>
2018-12-13 13:02:11 +0000

    Updates

---
+++ precache-manifest.8e035774b881bf4f2bef1398a28c0787.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a153da79ac95f2179ea",
+    "url": "/StudentExampleProjectGradeZero/static/js/runtime~main.8a153da7.js"
+  },
+  {
+    "revision": "0ef87688522eeaa9def6",
+    "url": "/StudentExampleProjectGradeZero/static/js/main.0ef87688.chunk.js"
+  },
+  {
+    "revision": "2afcd71389b3bd913e12",
+    "url": "/StudentExampleProjectGradeZero/static/js/1.2afcd713.chunk.js"
+  },
+  {
+    "revision": "bfee744ba82027472224d4a8844d41d0",
+    "url": "/StudentExampleProjectGradeZero/index.html"
+  }
+];
